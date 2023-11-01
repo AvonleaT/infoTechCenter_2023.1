28f35f110b0076296ae07ca73e885a6daebbb810
Thalmann <thalmann.avonlea@gmail.com>
2023-11-01 07:55:19 -0400

    Add listOfGasStations Function - Called & Stable

--- ProjectOragutan.py
+++ ProjectOragutan.py
@@ -8,7 +8,20 @@ from time import sleep
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
     currentGasLevel = random.choice(gasLevelList)
-    return currentGasLevel 
+    return currentGasLevel
+
+
+# Function with a list of GasStations
+def listOfGasStations():
+    gasStations = ["Shell","Speedway","Exxon","Meijer","Costco","Marathon","BP","Circle K","Wesco"]
+    gasStationsNearby = random.choice(gasStations)
+    return gasStationsNearby
+
+
+
+
+
+
 
 
 
